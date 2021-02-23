9e8bcbe06ce372d396c1a727f2b3e978db6438c5
Lenovo <anand9412868527@gmail.com>
2021-02-23 18:13:48 +0530

    dev#ch4

--- lib/main.dart
+++ lib/main.dart
@@ -15,31 +15,38 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.baseline,
-        children: [
-          Container(
-            width: 122,
-            height: 122,
-            alignment: Alignment.center,
-            color: Colors.red,
-          ),
-          Container(
-            width: 122,
-            height: 122,
-            alignment: Alignment.center,
-            color: Colors.green,
-          ),
-          Container(
-            width: 122,
-            height: 122,
-            alignment: Alignment.center,
-            color: Colors.blue,
-          ),
-        ],
-      )),
+      body: Container(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.indigo),
+              child: Container(
+                child: UserAccountsDrawerHeader(
+                  accountName: Text("steve"),
+                  accountEmail: Text("steve@gmaillcom"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1567336273898-ebbf9eb3c3bf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=486&q=80"),
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("personal"),
+              trailing: Icon(Icons.edit),
+            )
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
     );
   }
 }
